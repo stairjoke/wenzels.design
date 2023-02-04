3552914189afafa75ec54756ab0babe08891191a
Wenzel Massag <mail@wmassag.de>
2023-02-04 14:12:00 +0000

    added localization of global page headers

--- site/templates/default.php
+++ site/templates/default.php
@@ -40,9 +40,14 @@ echo ($locale);
 	<body>
 		<header>
 			<?php
+				// This templates uses the NavBase for localized site-wide content
+				// This is the .txt file directly in /[locale]/, it's name dictates
+				// the template used for the Home-page
+				$navBase = $kirby->page($locale);
+				
 				// Display different site-titles depending on availability
-				$hasTabletTitle = $site->title_tablet()->exists();
-				$hasMobileTitle = $site->title_mobile()->exists();
+				$hasTabletTitle = $navBase->header_title_tablet()->exists();
+				$hasMobileTitle = $navBase->header_title_mobile()->exists();
 
 				// If there is only one title display it to all viewport sizes
 				if(!$hasTabletTitle && !$hasMobileTitle) {
@@ -51,22 +56,22 @@ echo ($locale);
 				// If there is n additional mobile but no tablet title, use the
 				// desktop title for desktop and tablet, but mobile for mobile
 				}elseif(!$hasTabletTitle && $hasMobileTitle){
-					echo("<h1 class='desktop tablet'>".$site->title()."</h1>");
-					echo("<h1 class=mobile>".$site->title_mobile()."</h1>");
+					echo("<h1 class='desktop tablet'>".$navBase->header_title()."</h1>");
+					echo("<h1 class=mobile>".$navBase->header_title_mobile()."</h1>");
 
 				// If there is an additional title for tablet, but no mobile-
 				// title, use the desktop-title for desktop, and the tablet-
 				// title for tablet and mobile
 				}elseif($hasTabletTitle && !$hasMobileTitle) {
-					echo("<h1 class='desktop'>".$site->title()."</h1>");
-					echo("<h1 class='tablet mobile'>".$site->title_tablet()."</h1>");
+					echo("<h1 class='desktop'>".$navBase->header_title()."</h1>");
+					echo("<h1 class='tablet mobile'>".$navBase->header_title_tablet()."</h1>");
 
 				// If there is a desktop-, tablet-, and mobile-title, use them
 				// respectively
 				}elseif($hasTabletTitle && $hasMobileTitle){
-					echo("<h1 class=desktop>".$site->title()."</h1>");
-					echo("<h1 class=tablet>".$site->title_tablet()."</h1>");
-					echo("<h1 class=mobile>".$site->title_mobile()."</h1>");
+					echo("<h1 class=desktop>".$navBase->header_title()."</h1>");
+					echo("<h1 class=tablet>".$navBase->header_title_tablet()."</h1>");
+					echo("<h1 class=mobile>".$navBase->header_title_mobile()."</h1>");
 				}
 			?>
 			<nav>
@@ -85,13 +90,12 @@ echo ($locale);
 		
 				<ul class="menu triggered-by-nav-toggle">
 					<?php
-						$navBase = $kirby->page($locale);
 						echo("<li><a href='". $navBase->url() ."'>". $navBase->nav_title() ."</a></li>");
 						foreach($navBase->children() as $navItem){
 							echo("<li><a href='". $navItem->url() ."'>". $navItem->title() ."</a></li>");
 						}
 					?>
-					<li><a href="mailto:hello@wenzels.design?subject=Eierlegende Wollmilchsau&body=Hallo Wenzel!" role="button">Kontakt</a></li>
+					<li><a href="" role="button">Kontakt</a></li>
 				</ul>
 			</nav>
 		</header>
