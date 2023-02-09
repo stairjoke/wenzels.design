c71b86a02ed0b2d495fd490402116eca276cb99b
Wenzel Massag <mail@wmassag.de>
2023-02-09 20:46:04 +0000

    added ability to listen for optional css class-var

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
 <html lang="de-DE">
 	<head>
 		<meta charset="UTF-8">
 		
 		<!-- Using Kirby CSS feature -->
 		<?= css('assets/foundation.css') ?>
+		<?= (isset($class)) ? css("assets/$class.css") : ''; ?>
 		
 		<!-- CONTAINS A COLOR THAT CANNOT BE REFERENCED FROM CSS COLOR VARIABLES, NEEDS TO BE MAINTAINED! -->
 		<meta name="theme-color" media="(prefers-color-scheme: light)" content="#FCFDFB">
