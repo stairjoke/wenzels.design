234828bdcd77113587e70fdf52a3645995107a72
Wenzel Massag <mail@wmassag.de>
2023-02-09 19:15:55 +0000

    adding footer content

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,3 +1,6 @@
-		<footer>Footer Content goes here&hellip;</footer>
+		<footer>
+			<p><?= $site->footer() ?></p>
+			<p><a href="<?= page('imprint')->url() ?>"><?= page('imprint')->title() ?>,</a> und <a href="<?= page('privacy')->url() ?>"><?= page('privacy')->title() ?></a></p>
+		</footer>
 	</body>
 </html>
