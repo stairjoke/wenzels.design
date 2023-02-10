11172f8fe83e3ea92eccac75fd155cc886873807
Wenzel Massag <mail@wmassag.de>
2023-02-10 10:15:50 +0000

    added layout for home page

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,13 +1,18 @@
 <?= snippet('header', ['class' => 'home']) ?>
 <main class="home">
-	<article class="featured">
-		<?= $page->card_text()->toBlocks() ?>
-		<?= $page->button_label() ?>
+	<article class="CTA">
+		<div>
+			<?= $page->card_text()->toBlocks() ?>
+			<a class="button cta" href="<?= ($page->button_url()->exists()) ? $page->button_url() : $site->Contact_Link() ?>"><?= $page->button_label() ?></a>
+		</div>
 	</article>
 <?php foreach(page('projects')->children() as $project): ?>
 	<article>
 		<h3><?= $project->title() ?></h3>
-		<?= $project->image() ?>
+		<div class=card-body>
+			<?= $project->image() ?>
+			<p><?= $project->tagline() ?></p>
+		</div>
 	</article>
 <?php endforeach;	?>
 </main>
