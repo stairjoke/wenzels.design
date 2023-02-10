55800665d1272e53696229f6961f40680ce6517c
Wenzel Massag <mail@wmassag.de>
2023-02-10 11:03:20 +0000

    only showing listed project on home

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,7 +6,7 @@
 			<a class="button cta" href="<?= ($page->button_url()->exists()) ? $page->button_url() : $site->Contact_Link() ?>"><?= $page->button_label() ?></a>
 		</div>
 	</article>
-<?php foreach(page('projects')->children() as $project): ?>
+<?php foreach(page('projects')->children()->listed() as $project): ?>
 	<article><a class="card" href="<?= $project->url() ?>">
 		<h3><?= $project->title() ?></h3>
 		<div class=card-body>
