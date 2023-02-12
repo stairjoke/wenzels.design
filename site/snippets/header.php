3001118e5d7e9a71f7f9fd65fa06b6c9f2ac8125
Wenzel Massag <mail@wmassag.de>
2023-02-12 18:28:53 +0000

    viewport size error

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,7 @@
 <html lang="de-DE">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
 		<!-- Using Kirby CSS feature -->
 		<?= css('assets/foundation.css') ?>
